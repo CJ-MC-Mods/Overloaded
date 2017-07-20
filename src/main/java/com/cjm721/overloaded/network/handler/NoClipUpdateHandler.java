8b38977793527f862d43a7b4b35179ac2c8651c8
CJ Miller <cjmiller721@gmail.com>
2017-07-20 00:50:09 -0700

    Updated Version and fixed Race Condition

--- src/main/java/com/cjm721/overloaded/network/handler/NoClipUpdateHandler.java
+++ src/main/java/com/cjm721/overloaded/network/handler/NoClipUpdateHandler.java
@@ -1,12 +1,9 @@
 package com.cjm721.overloaded.network.handler;
 
-import com.cjm721.overloaded.item.ModItems;
 import com.cjm721.overloaded.item.functional.armor.ArmorEventHandler;
-import com.cjm721.overloaded.network.packets.MultiToolRightClickMessage;
 import com.cjm721.overloaded.network.packets.NoClipStatusMessage;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.text.TextComponentString;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -25,8 +22,10 @@ public class NoClipUpdateHandler implements IMessageHandler<NoClipStatusMessage,
 
     @SideOnly(Side.CLIENT)
     private void clientSide(NoClipStatusMessage message) {
-        ArmorEventHandler.setNoClip(Minecraft.getMinecraft().player,message.isEnabled());
-        Minecraft.getMinecraft().player.sendStatusMessage(new TextComponentString("No Clip: " + message.isEnabled()), true);
+        Minecraft.getMinecraft().addScheduledTask(() -> {
+            ArmorEventHandler.setNoClip(Minecraft.getMinecraft().player,message.isEnabled());
+            Minecraft.getMinecraft().player.sendStatusMessage(new TextComponentString("No Clip: " + message.isEnabled()), true);
+        });
     }
 
 }
