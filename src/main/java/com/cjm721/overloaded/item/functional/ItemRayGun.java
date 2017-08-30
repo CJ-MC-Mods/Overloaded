6aca0683496e9c1aec25ef8e96e13fcc9c196ed2
CJ Miller <cjmiller721@gmail.com>
2017-08-29 23:13:12 -0700

    working on ray gun

--- src/main/java/com/cjm721/overloaded/item/functional/ItemRayGun.java
+++ src/main/java/com/cjm721/overloaded/item/functional/ItemRayGun.java
@@ -1,4 +1,36 @@
 package com.cjm721.overloaded.item.functional;
 
-public class ItemRayGun {
+import com.cjm721.overloaded.OverloadedCreativeTabs;
+import com.cjm721.overloaded.client.render.dynamic.general.ResizeableTextureGenerator;
+import com.cjm721.overloaded.config.OverloadedConfig;
+import com.cjm721.overloaded.item.ModItem;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import static com.cjm721.overloaded.Overloaded.MODID;
+
+public class ItemRayGun extends ModItem {
+
+    public ItemRayGun() {
+        setMaxStackSize(1);
+        setRegistryName("ray_gun");
+        setUnlocalizedName("ray_gun");
+        setCreativeTab(OverloadedCreativeTabs.TECH);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerModel() {
+        ModelResourceLocation location = new ModelResourceLocation(new ResourceLocation(MODID, "ray_gun"), null);
+        ModelLoader.setCustomModelResourceLocation(this, 0, location);
+
+        ResizeableTextureGenerator.addToTextureQueue(new ResizeableTextureGenerator.ResizableTexture(
+                new ResourceLocation(MODID, "textures/items/zapper.png"),
+                new ResourceLocation(MODID, "textures/dynamic/items/zapper.png"),
+                OverloadedConfig.textureResolutions.blockResolution));
+    }
+
 }
