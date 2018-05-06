bf6e300feeb623f984272686c642ed6748bd37d2
CJ Miller <cjmiller721@gmail.com>
2018-05-06 16:22:19 -0700

    Settings Editor model

--- src/main/java/com/cjm721/overloaded/item/functional/ItemSettingEditor.java
+++ src/main/java/com/cjm721/overloaded/item/functional/ItemSettingEditor.java
@@ -2,27 +2,32 @@ package com.cjm721.overloaded.item.functional;
 
 import com.cjm721.overloaded.Overloaded;
 import com.cjm721.overloaded.OverloadedCreativeTabs;
+import com.cjm721.overloaded.client.render.dynamic.general.ResizeableTextureGenerator;
+import com.cjm721.overloaded.config.OverloadedConfig;
 import com.cjm721.overloaded.item.ModItem;
 import com.cjm721.overloaded.network.OverloadedGuiHandler;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
+
+import static com.cjm721.overloaded.Overloaded.MODID;
 
 public class ItemSettingEditor extends ModItem {
 
     public ItemSettingEditor() {
+        setMaxStackSize(1);
         setRegistryName("settings_editor");
         setUnlocalizedName("settings_editor");
         setCreativeTab(OverloadedCreativeTabs.TECH);
-    }
-
-    @Override
-    public void registerModel() {
-
     }
 
     @Override
@@ -33,5 +38,18 @@ public class ItemSettingEditor extends ModItem {
         }
 
         return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
+
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerModel() {
+        ModelResourceLocation location = new ModelResourceLocation(new ResourceLocation(MODID, "settings_editor"), null);
+        ModelLoader.setCustomModelResourceLocation(this, 0, location);
+
+        ResizeableTextureGenerator.addToTextureQueue(new ResizeableTextureGenerator.ResizableTexture(
+                new ResourceLocation(MODID, "textures/items/settings_editor.png"),
+                new ResourceLocation(MODID, "textures/dynamic/items/settings_editor.png"),
+                OverloadedConfig.textureResolutions.blockResolution));
     }
 }
