19cc97a658f9eb3855856832a2ba595f9309ad0b
CJ Miller <cjmiller721@gmail.com>
2018-05-06 18:32:16 -0700

    Fix Compressed Blocks not having the correct blast resistance.

--- src/main/java/com/cjm721/overloaded/block/compressed/BlockCompressed.java
+++ src/main/java/com/cjm721/overloaded/block/compressed/BlockCompressed.java
@@ -15,6 +15,7 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.IStateMapper;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -25,6 +26,7 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -146,6 +148,11 @@ public class BlockCompressed extends ModBlock {
         }
 
         return hardness;
+    }
+
+    @Override
+    public float getExplosionResistance(World world, BlockPos pos, @Nullable Entity exploder, Explosion explosion) {
+        return getBlockHardness(world.getBlockState(pos), world,pos)/ 5.0F;
     }
 
     @Override
