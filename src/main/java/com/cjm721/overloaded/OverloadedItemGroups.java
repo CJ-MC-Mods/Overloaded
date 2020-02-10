b3a4f637f92d5fbe6185cc8cbbe6080dcce0117b
CJ Miller <github@cjm721.com>
2020-02-09 20:19:35 -0800

    Sort item group so easier to find things

--- src/main/java/com/cjm721/overloaded/OverloadedItemGroups.java
+++ src/main/java/com/cjm721/overloaded/OverloadedItemGroups.java
@@ -3,6 +3,9 @@ package com.cjm721.overloaded;
 import com.cjm721.overloaded.block.ModBlocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
+import java.util.Comparator;
 
 public class OverloadedItemGroups {
 
@@ -11,6 +14,13 @@ public class OverloadedItemGroups {
         @Override
         public ItemStack createIcon() {
           return new ItemStack(ModBlocks.creativeGenerator);
+        }
+
+        @Override
+        public void fill(NonNullList<ItemStack> items) {
+          super.fill(items);
+
+          items.sort(Comparator.<ItemStack, String>comparing(is -> is.getDisplayName().toString()));
         }
       };
 }
