70aa27f5a6064e11a72916febf4b3f38b8ffb5f6
CJ Miller <github@cjm721.com>
2025-05-26 00:52:44 -0500

    Player interface saves who placed it again

--- src/main/java/com/cjm721/overloaded/tile/functional/TilePlayerInterface.java
+++ src/main/java/com/cjm721/overloaded/tile/functional/TilePlayerInterface.java
@@ -2,10 +2,17 @@ package com.cjm721.overloaded.tile.functional;
 
 import com.cjm721.overloaded.tile.ModTiles;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.Nonnull;
 import java.util.Optional;
@@ -19,40 +26,37 @@ public class TilePlayerInterface extends BlockEntity {
     super(ModTiles.playerInterface.get(), pos, blockState);
   }
 
-  //  @Override
-  //  @Nonnull
-  //  public CompoundTag getUpdateTag() {
-  //    return save(new CompoundTag());
-  //  }
-  //
-  //  @Nullable
-  //  @Override
-  //  public SUpdateTileEntityPacket getUpdatePacket() {
-  //    CompoundTag tag = new CompoundTag();
-  //    save(tag);
-  //
-  //    return new SUpdateTileEntityPacket(getBlockPos(), 1, tag);
-  //  }
-  //
-  //  @Override
-  //  public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
-  //    this.load(this.getBlockState(), pkt.getTag());
-  //  }
-  //
-  //  @Override
-  //  public void load(@Nonnull BlockState state, @Nonnull CompoundTag compound) {
-  //    if (compound.contains("Placer")) placer = UUID.fromString(compound.getString("Placer"));
-  //
-  //    super.load(state, compound);
-  //  }
-  //
-  //  @Override
-  //  @Nonnull
-  //  public CompoundTag save(@Nonnull CompoundTag compound) {
-  //    if (placer != null) compound.putString("Placer", placer.toString());
-  //
-  //    return super.save(compound);
-  //  }
+  @Override
+  public CompoundTag getUpdateTag(HolderLookup.Provider registries) {
+    CompoundTag tag = new CompoundTag();
+    saveAdditional(tag, registries);
+    return tag;
+  }
+
+  @Override
+  public @Nullable Packet<ClientGamePacketListener> getUpdatePacket() {
+    return ClientboundBlockEntityDataPacket.create(this);
+  }
+
+  @Override
+  public void onDataPacket(
+      Connection net, ClientboundBlockEntityDataPacket pkt, HolderLookup.Provider lookupProvider) {
+    this.loadAdditional(pkt.getTag(), lookupProvider);
+  }
+
+  @Override
+  protected void loadAdditional(CompoundTag compound, HolderLookup.Provider registries) {
+    if (compound.contains("Placer")) placer = UUID.fromString(compound.getString("Placer"));
+
+    super.loadAdditional(compound, registries);
+  }
+
+  @Override
+  protected void saveAdditional(CompoundTag compound, HolderLookup.Provider registries) {
+    if (placer != null) compound.putString("Placer", placer.toString());
+
+    super.saveAdditional(compound, registries);
+  }
 
   public void setPlacer(@Nonnull LivingEntity placer) {
     if (placer instanceof Player) this.placer = placer.getUUID();
